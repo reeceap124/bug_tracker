16c0692c9b01a3bbffb89a5127fe613c51db47cf
Reece Pierson <piersonreece@gmail.com>
2020-08-06 19:29:44 -0500

    sets up basic file structure

--- src/components/dash/Dash.js
+++ src/components/dash/Dash.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {axiosAuth} from '../../util/axiosAuth'
 
 const Dash = () => {
+    const [issues, setIssues] = useState()
+    useEffect(()=>{
+        axiosAuth().get('/issues/1')
+        .then(res => {
+            setIssues(res.data)
+        })
+        .catch(()=>{
+            console.log('There was an issue retrieving your issues')
+        })
+    }, [])
+    console.log('issues', issues)
     return (
         <h1>Hello from the dash</h1>
     )
