61a060be391e34e51577d7ac8d61835e0a20bc53
Reece Pierson <piersonreece@gmail.com>
2020-09-15 17:23:26 -0500

    further planning for issues context

--- src/contexts/IssuesContext.js
+++ src/contexts/IssuesContext.js
@@ -1,8 +1,12 @@
-import React, {createContext, useContext} from 'react';
+import React, {createContext, useContext, useState} from 'react';
 const IssuesContext = createContext();
 export function useIssues() { //Custom hook for consuming context
     return useContext(IssuesContext)
 }
+//Set up local state for issues here. 
+    //issues
+    //filtered
+    //active
 
 //Creates context that includes the full issue list, the filtered issues, and the active issue 
 export function IssuesProvider({issues, filteredIssues, activeIssue, children}) {
