941cf50e6c1611be438c10e62ac4daa786ad2131
Reece Pierson <piersonreece@gmail.com>
2020-09-16 19:27:47 -0500

    adds a bit more to issues context

--- src/contexts/IssuesContext.js
+++ src/contexts/IssuesContext.js
@@ -10,6 +10,12 @@ export function useIssues() { //Custom hook for consuming context
 
 //Creates context that includes the full issue list, the filtered issues, and the active issue 
 export function IssuesProvider({issues, filteredIssues, activeIssue, children}) {
+    const [issuesList, setIssuesList] = useState(issues)
+    const [filteredList, setFilteredList] = useState(filteredIssues)
+    const [active, setActive] = 
+    function updateIssues(newIssuesList) {
+        setIssuesList(newIssuesList)
+    }
     return (
         <IssuesContext.Provider value={{issues, filteredIssues, activeIssue}}>
             {children}
