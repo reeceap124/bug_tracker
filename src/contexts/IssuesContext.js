22965dbf3851421044c3037ee6e482957a639551
Reece Pierson <piersonreece@gmail.com>
2020-09-23 15:26:05 -0500

    restructure issues context to focus just on one list of issues

--- src/contexts/IssuesContext.js
+++ src/contexts/IssuesContext.js
@@ -3,40 +3,14 @@ const IssuesContext = createContext();
 export function useIssues() { //Custom hook for consuming context
     return useContext(IssuesContext)
 }
-//Set up local state for issues here. 
-    //issues
-    //filtered
-    //active
 
-//Creates context that includes the full issue list, the filtered issues, and the active issue 
-const ACTIONS = {
-    updateIssues : 'updateIssues',
-    updateFiltered : 'updateFiltered',
-    updateActive : 'updateActive'
-}
-//Reducer for context
-function reducer (state, action) {
-    switch(action.type) {
-        case ACTIONS.updateIssues:
-            return {...state, allIssues: action.payload}
-        case ACTIONS.updateFiltered:
-            return {...state, updateFiltered: action.payload}
-        case ACTIONS.updateActive:
-            return {...state, updateActive:action.payload}
-    }
-}
 export function IssuesProvider({children}) {
-
-    const [issuesState, dispatch] = useReducer(reducer, {
-        allIssues,
-        filteredIssues,
-        activeIssue
-    })
-    function update(actionType, payload){
-        dispatch({type: ACTIONS[actionType], payload: payload})
+    const [issues, setIssues] = useState([])
+    function updateIssues(newIssuesList){
+        setIssues(newIssuesList)
     }
     return (
-        <IssuesContext.Provider value={{issues:issuesState.allIssues, filteredIssues:issuesState.filteredIssues, activeIssue:issuesState.filteredIssues, update}}>
+        <IssuesContext.Provider value={{issues, updateIssues}}>
             {children}
         </IssuesContext.Provider>
     )
