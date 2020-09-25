28b9d5f3a7da56c90371a145f72feaef47715a32
Reece Pierson <piersonreece@gmail.com>
2020-09-24 20:35:52 -0500

    adds onto issues context, allows updates, and setup in issuesList

--- src/components/dash/issueList/IssueList.js
+++ src/components/dash/issueList/IssueList.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import IssueCard from './IssueCard'
 import ModalSelect from '../modals/ModalSelect'
+import {useUser} from '../../../contexts/UserContext'
+import {useIssues} from '../../../contexts/IssuesContext'
+import { axiosAuth } from '../../../util/axiosAuth';
 
 const IssueList = (props) => {
+    const id = useUser()
+    const issuesContext = useIssues()
+    useEffect(()=>{
+        axiosAuth().get(`/issues/list/${id}`)
+        .then(res=>{
+            if (res.data.error){
+                return console.error(res.data)
+            }
+            issuesContext.update(issuesContext.filtered, res.data)
+        })
+        .catch(err=>{
+            console.error('There was an error fetching your issues', err)
+        })
+    }, [id])
     function cardClick(issueId) {
         props.setActiveIssue(issueId)
     }
@@ -11,7 +28,7 @@ const IssueList = (props) => {
             <div className='modalSelect'>
                 <ModalSelect/>
             </div>
-            {props.list.map(issue=>{
+            {issuesContext.filtered.map(issue=>{
                 return <IssueCard key={issue.id} issue={issue} cardClick={cardClick}/>
             })}
         </section>
